df336db41cbf1fe7ea4f34a3a51d0553f8ff4b1f
Aakash <Aakash@AAKASH>
2016-11-03 21:04:50 -0700

    Initial Change 4th nov 12:4am

--- Assignment2_COMP_229_300808436/Assignment2_COMP_229_300808436/myPages/Site1master.Master.designer.cs
+++ Assignment2_COMP_229_300808436/Assignment2_COMP_229_300808436/myPages/Site1master.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Assignment2_COMP_229_300808436.myPages {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
--- Assignment2_COMP_229_300808436/Assignment2_COMP_229_300808436/myPages/index.aspx
+++ Assignment2_COMP_229_300808436/Assignment2_COMP_229_300808436/myPages/index.aspx
@@ -1,5 +1,7 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/myPages/Site1master.Master" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="Assignment2_COMP_229_300808436.myPages.index" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <p>
-    &nbsp;</p>
+    <br/>
+    
+    <div style="text-align: center" >OUR COMPANY SKY MUSICALS PROVIDES YOU THE BEST MUSICAL AUTO PARTS<br/> 
+        AND MUSICAL INSTRUMENTS YOU NEED FOR AMAZING CONCERTS AND MUSICAL EXPERIENCE</div>
 </asp:Content>
